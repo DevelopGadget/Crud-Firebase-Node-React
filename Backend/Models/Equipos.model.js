ed3d54998ba0e4b745326bb494c838554713d49c
DevelopGadget <fernandoaraujo081@gmail.com>
2019-03-29 12:24:09 -0500

    model

--- Backend/Models/Equipos.model.js
+++ Backend/Models/Equipos.model.js
@@ -1,1 +1,26 @@
 import { Joi, celebrate } from 'celebrate';
+
+// Variables para usar despues
+const Header = {
+    headers: Joi.object().keys({
+        Id: Joi.string().uuid().required()
+    })
+};
+
+const Body = {
+    body: Joi.object().keys({
+        Nombre: Joi.string().min(1).max(30).required(),
+        Estadio: Joi.string().min(1).max(30).required(),
+        UrlEscudo: Joi.string().min(1).max(200).required().uri({})
+    })
+};
+
+//Modelo que valido el id de los headers
+export const Id = celebrate(Header);
+
+//Modelo que valido un nuevo equipo
+export const Equipo = celebrate(Body);
+
+//Modelo con que valido una modificacion
+export const Modificacion = celebrate(Object.assign(Header, Body));
+
