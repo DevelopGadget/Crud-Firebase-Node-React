30aec409747f664f5629247fff9b7e70d4885517
DevelopGadget <fernandoaraujo081@gmail.com>
2019-04-01 12:09:00 -0500

    Updates

---
+++ precache-manifest.13469474c749961e04c5e2d170a827d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e30e77c1d819b108a7d",
+    "url": "/static/css/main.eaf67821.chunk.css"
+  },
+  {
+    "revision": "0e30e77c1d819b108a7d",
+    "url": "/static/js/main.edc89de9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a5206aa84b0924f65a1b",
+    "url": "/static/js/2.a9e44b6b.chunk.js"
+  },
+  {
+    "revision": "2f9e6c00fa3c7bf97971d46bbdcdfb2c",
+    "url": "/index.html"
+  }
+];
